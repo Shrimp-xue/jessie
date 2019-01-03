7bf5df18764b4f899a60b180477ba92306085d58
shrimp <shrimp_xue@163.com>
2019-01-03 17:19:15 +0800

    初始化项目

---
+++ src/main/Java/com/jessie/common/callback/MethodCallback.java
@@ -0,0 +1,13 @@
+package com.jessie.common.callback;
+
+/**
+ * @program: jessie
+ * @description: 方法回调接口
+ * @author: Shrimp
+ * @create: 2018-12-21 09:03
+ **/
+public interface MethodCallback {
+
+    Object execute(Object... params);
+
+}
